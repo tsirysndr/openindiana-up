bd66364e75783d1e3642dcc45315a442ed0e73e1
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2025-11-01 19:11:33 +0300

    Update size option description for VM disk image creation to include example

--- main.ts
+++ main.ts
@@ -173,7 +173,7 @@ if (import.meta.main) {
     )
     .option(
       "--size <size:string>",
-      "Size of the VM disk image to create if it does not exist",
+      "Size of the VM disk image to create if it does not exist (e.g., 20G)",
       {
         default: "20G",
       },
